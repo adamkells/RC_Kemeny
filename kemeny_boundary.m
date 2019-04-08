37d9b6dd189e27fe09a090248fc00fda58088b05
adamkells <adam.kells@ucdconnect.ie>
2019-04-09 00:50:17 +0100

    added diffusive

--- kemeny_boundary.m
+++ kemeny_boundary.m
@@ -1,10 +1,10 @@
-function [kemenyR]=kemeny_boundary(K,INV_K,eq,boundary,tmp2)
+function [kemenyR]=kemeny_boundary(K,INV_K,eq,A)
 
 N=size(K,1);
-A=zeros(N,3);
-A(tmp2(1:boundary(1)),1)=1;
-A(tmp2(boundary(1)+1:boundary(2)),2)=1;
-A(tmp2(boundary(2)+1:end),3)=1;
+% A=zeros(N,3);
+% A(tmp2(1:boundary(1)),1)=1;
+% A(tmp2(boundary(1)+1:boundary(2)),2)=1;
+% A(tmp2(boundary(2)+1:end),3)=1;
 [R,P_EQ,Aclus]=hummer_szabo_clustering_A(K',INV_K, eq, A);
 
 % analyse eigenvalues vectors of clustered matrix R
