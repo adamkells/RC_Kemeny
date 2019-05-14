3924e4185e429329655c5102ff8edd865e37f976
Adam Kells <adam.kells@ucdconnect.ie>
2019-05-14 15:10:37 +0100

    finished working code whoop whoop

--- conv_check.m
+++ conv_check.m
@@ -1,10 +1,13 @@
 for ii=0:1
-    for jj=0:2
+    for jj=[0,2]     
         [ii,jj]
-        for choice=1:6
+        for choice=1:4
             %choice
             %keyboard
             [kemeny_l(choice),Rtmp]=kemeny_boundary(K,INV_K,eq,cc(:,:,choice),ii,jj);
+        end
+        if jj==2
+            jj=1;
         end
         opt = (ii*3)+(jj+1)
         [a,b]=max(kemeny_l);
